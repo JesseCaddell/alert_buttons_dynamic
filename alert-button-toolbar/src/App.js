1dd1f19347e4feb4958918ec727d91c34defadd9
Jesse <jesse.caddell@wsu.edu>
2025-05-20 21:22:50 -0700

    Initial commit

---
+++ alert-button-toolbar/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Toolbar from './Toolbar';
+
+function App() {
+  return (
+      <div className="App" style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <h1>Alert Button Toolbar</h1>
+        <Toolbar />
+      </div>
+  );
+}
+
+export default App;
